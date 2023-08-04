dd209b6b0c1cdda62bd687ad543feb63427ea320
Your Name <yhoshoa144@gmail.com>
2023-08-04 17:58:46 +0300

    a

---
+++ JWT/Controllers/UserController.cs
@@ -0,0 +1,25 @@
+﻿using JWT.Models;
+using JWT.services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JWT.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly repository repository;
+
+        public UserController(repository repository)
+        {
+            this.repository = repository;
+        }
+        [HttpPost]
+        public async Task<ActionResult<User>>Register(UserDto user)
+        {
+            repository.CreatePasswordHash(user.Password,out byte[]passwordHash, out byte[] passwordSalt)
+        }
+    }
+}
---
+++ JWT/services/Repository.cs
@@ -0,0 +1,25 @@
+﻿using JWT.Data;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace JWT.services
+{
+    public class repository
+    {
+        private readonly UserContext context;
+
+        public repository(UserContext context)
+        {
+            this.context = context;
+        }
+
+        public void CreatePasswordHash(string password, out byte[]passwordHash,out byte[]passwordSalt)
+        {
+            using(var hmac = new HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash=hmac.ComputeHash(Encoding.UTF8.GetBytes(password));    
+            }
+        }
+    }
+}
